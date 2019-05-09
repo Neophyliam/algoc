d2bffe6e150831b76722c28528a3dbe755ceae63
Neophyliam <727549953@qq.com>
2019-05-09 23:13:55 +0800

    add cute minunit.h

---
+++ src/minunit.h
@@ -0,0 +1,14 @@
+#ifndef MINUNIT_H_HXJE4TUL
+#define MINUNIT_H_HXJE4TUL
+
+#define mu_assert(message, test) do { \
+    if (!(test)) return message;} \
+    while (0)
+#define mu_run_test(test) do { \
+    char *message = test(); \
+    tests_run++; \
+    if (message) return message; \
+    } while(0)
+extern int tests_run;
+
+#endif /* end of include guard: MINUNIT_H_HXJE4TUL */
